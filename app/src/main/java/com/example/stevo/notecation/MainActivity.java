908ca3465d2f83fb28ff6479f9234986523d66ea
Ptheven <sjconnolly520@gmail.com>
2015-04-11 10:28:00 -0700

    Take my code

--- app/src/main/java/com/example/stevo/notecation/MainActivity.java
+++ app/src/main/java/com/example/stevo/notecation/MainActivity.java
@@ -1,18 +1,68 @@
 package com.example.stevo.notecation;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
+
+
+    SeekBar distanceSlider;
+    Button clicker;
+    TextView distanceValue;
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initializeVars();
+
+
+        distanceValue.setText(distanceSlider.getProgress()+ " YDs");
+
+
+        distanceSlider.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            int progressChange = 0;
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                Log.d("debug", "Progress = " + progress);
+                progressChange = progress;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                distanceValue.setText(progressChange + " YDs");
+            }
+        });
     }
+
+    //ChangeMessage on Button click
+    public void ChangeMessage(View view){
+
+        intent = new Intent(this, map_activity.class);
+        startActivity(intent);
+
+    }
+
 
 
     @Override
@@ -36,4 +86,11 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void initializeVars() {
+        distanceSlider = (SeekBar) findViewById(R.id.seekBar);
+        clicker = (Button) findViewById(R.id.main_button);
+        distanceValue = (TextView) findViewById(R.id.textView3);
+    }
+
 }
---
+++ app/src/main/java/com/example/stevo/notecation/MapThing.java
@@ -0,0 +1,10 @@
+package com.example.stevo.notecation;
+
+/**
+ * Created by Stevo on 4/11/15.
+ */
+public class MapThing {
+
+
+
+}
